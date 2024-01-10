7546894e78c5bddd878903f1fea7a8a6d6b4cca3
Nagathi <ragbr7070@gmail.com>
2024-01-10 18:21:49 -0300

    Algumas alterações de conteúdo

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,14 +1,26 @@
 export const dataFake = [
   {
     "id":"1",
-    "title": "NOVO HOMEM DE FERRO EM 3D",
-    "description": "marvel anuncia um novo filme do homem de ferro, confira",
-    "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7F51FA9F6CBD9F0C9B1394B1CC0A6A842D07091318674E234CD33CBF7C28CDC3/scale?width=1200&aspectRatio=1.78&format=jpeg"
+    "title": "Satoru Gojo está morto",
+    "description": "Infelizmente o Goat foi roubado",
+    "photoCover":"https://i0.wp.com/metagalaxia.com.br/wp-content/uploads/2023/09/Gojo-vivo.jpg?resize=1024%2C611&ssl=1"
   },
   {
     "id":"2",
-    "title": "Nova Série anunciada no Disney +",
-    "description": "bla blabla",
-    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+    "title": "Capítulo 238 de Jujutsu Kaisen confirma a morte de um personagem popular",
+    "description": "Kashimo Hajime foi morto por Ryomen Sukuna",
+    "photoCover":"https://criticalhits.com.br/wp-content/webp-express/webp-images/doc-root/wp-content/uploads/2023/10/Jujutsu-Kaisen_-After-Gojou-whos-696x392.jpg.webp"
+  },
+  {
+    "id":"3",
+    "title": "Jujutsu Kaisen 0",
+    "description": "Filme é dublado e está disponível nas principais mídias",
+    "photoCover":"https://cdn.ome.lt/1SDJoI-H5odCm0zL_DMO8qksfU4=/1200x630/smart/extras/conteudos/jujtusu_kaisen_0.png"
+  },
+  {
+    "id":"4",
+    "title": "Capítulo 250: Previsões",
+    "description": "Espero que o Sukuna perca",
+    "photoCover":"https://blogdataverna.com.br/wp-content/uploads/2023/07/kpnl802eroz51-752x440.jpg"
   }
 ]
